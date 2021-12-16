4509a3d648abb4a94a2515bcaf2967835a8500c4
PaxPumpkin <OhNoYouDont@GetBent.edu>
2021-12-16 11:14:04 -0500

    2021 Day 15 - Chiton -  I got it, but my algorithm doesn't seem to match what other people are doing. or at least not exactly. But I HATE pathfinding. I suck at it. Needs Mild Refactor 2021 Day 16 - PacketDecoder - This needs HEAVY refactoring. I just don't have the time today.

---
+++ AoC_2021/Day15_Chiton.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AoC_2021
+{
+   class Day15_Chiton : AoCodeModule
+   {
+      public Day15_Chiton()
+      {
+         inputFileName = @"InputFiles\" + this.GetType().Name + ".txt";
+         GetInput();
+         OutputFileReset();
+      }
+      public override void DoProcess()
+      {
+         //** > Result for Day15_Chiton part 1: Lowest Risk Path is 696 (Process: 172.4869 ms)
+         //** > Result for Day15_Chiton part 2: Lowest Risk Path is 2952 (Process: 16166.9035 ms)
+         ResetProcessTimer(true);
+         CavernSpot.LoadInput(inputFile);
+         AddResult("Lowest Risk Path is " + CavernSpot.Answer);
+         ResetProcessTimer(true);
+         CavernSpot.Widify(inputFile, 5); // can probably reuse this instead of the standard LoadInput Wide-ification Factor of 5
+         AddResult("Lowest Risk Path is " + CavernSpot.Answer);
+         ResetProcessTimer(true);
+      }
+   }
+   public class CavernSpot
+   {
+      public int X { get; set; }
+      public int Y { get; set; }
+      public int RiskCost { get; set; }
+      public int LowestCostFromHere { get; set; }
+      public int ManhattanDistance { get { return X + Y; } }
+      private List<CavernSpot> MyNeighbors = new List<CavernSpot>();
+
+      public static Dictionary<PointDescription, CavernSpot> CavernSpotDictionary = new Dictionary<PointDescription, CavernSpot>();
+      public static int HighestX { get; set; }
+      public static int HighestY { get; set; }
+      public static string Answer { get { return CavernSpotDictionary[new PointDescription { X = 0, Y = 0 }].LowestCostFromHere.ToString(); } }
+
+      public static Queue<CavernSpot> queuedCostResolutions = new Queue<CavernSpot>();
+      public CavernSpot(int x, int y, int risk)
+      {
+         X = x; Y = y; RiskCost = risk;
+         CavernSpotDictionary.Add(new PointDescription { X=x, Y=y }, this);
+      }
+      public void LoadNeighbors()
+      {
+         MyNeighbors.Clear();
+         SetSpot(X, Y - 1);
+         SetSpot(X, Y + 1);
+         SetSpot(X + 1, Y);
+         SetSpot(X - 1, Y);
+      }
+
+      private void SetSpot(int x, int y)
+      {
+         PointDescription toGet = new PointDescription { X = x, Y = y };
+         if (CavernSpotDictionary.ContainsKey(toGet))
+            MyNeighbors.Add(CavernSpotDictionary[toGet]);
+      }
+      public void ResolveCost()
+      {
+         if (X == HighestX && Y == HighestY) // end point, the cost is the stated value. 
+            LowestCostFromHere = RiskCost;
+         else
+         {
+            int lowestCost;
+            LowestCostFromHere = int.MaxValue;
+            //iterate through neighbors and find the one with the lowest cost to reach the end. 
+            // if they are unresolved values, save for resolving after. 
+            MyNeighbors.OrderByDescending(spot => spot.ManhattanDistance).ToList().ForEach(spot =>
+             {
+                if (spot.LowestCostFromHere == 0) //not already solved. 
+                {
+                   queuedCostResolutions.Enqueue(spot); // because recursion is deadly at 250,000 feet.
+                }
+                else
+                { // of the ones that actually have a solved value, get the lowest one. Add spots' own cost. 
+                   // the "cost" of start is always zero. 
+                   lowestCost = spot.LowestCostFromHere + ((X == 0 && Y == 0) ? 0 : RiskCost);
+                   LowestCostFromHere = Math.Min(LowestCostFromHere, lowestCost);
+                }
+             });
+         }
+      }
+
+      public static void LoadInput(List<string> inputFile) // refactor into Wide-ifier. With default loop factor of 1
+      {
+         CavernSpot.CavernSpotDictionary.Clear();
+         HighestX = inputFile[0].Length - 1;
+         HighestY = inputFile.Count - 1;
+         for (int y = 0; y <= HighestY; y++)
+            for (int x = 0; x <= HighestX; x++)
+               new CavernSpot(x, y, int.Parse(inputFile[y][x].ToString()));
+
+         FillConnectors();
+         LoadCosts();
+      }
+      public static void Widify(List<string> inputFile, int WidificationFactor = 1) //Wide-ification by PaxTech™
+      {
+         CavernSpot.CavernSpotDictionary.Clear();
+         HighestX = (inputFile[0].Length * 5) - 1;
+         HighestY = (inputFile.Count * 5) - 1;
+         int xInterval = inputFile[0].Length;
+         int yInterval = inputFile.Count;
+         for (int y = 0; y <= inputFile.Count - 1; y++)
+            for (int x = 0; x <= inputFile[0].Length - 1; x++)
+               for (int ywidify = 0; ywidify < WidificationFactor; ywidify++)
+                  for (int widify = 0; widify < WidificationFactor; widify++)
+                  {
+                     int inputNumber = int.Parse(inputFile[y][x].ToString());
+                     int widification = widify + ywidify;
+                     int widifyNumber = (inputNumber + widification) % 9;
+                     widifyNumber = widifyNumber == 0 ? 9 : widifyNumber;
+                     int xModification = x + (widify * xInterval);
+                     int yModification = y + (ywidify * yInterval);
+                     new CavernSpot(xModification, yModification, widifyNumber);
+                  }
+
+         FillConnectors();
+         LoadCosts();
+      }
+      private static void FillConnectors()
+      {
+         CavernSpotDictionary.Keys.ToList().ForEach(key => CavernSpotDictionary[key].LoadNeighbors());
+      }
+      private static void LoadCosts()
+      {
+         int leastCost, lastCost;
+         lastCost = 0;
+         // first time only queues empty spaces and sets basic info.
+         CavernSpotDictionary.OrderByDescending(spot => spot.Value.ManhattanDistance).ToList().ForEach(spot => spot.Value.ResolveCost()); // calls to ResolveCost can generate queued entries.
+         leastCost = CavernSpotDictionary[new PointDescription { X = 0, Y = 0 }].LowestCostFromHere;
+         while (queuedCostResolutions.Count > 0 || !(leastCost == lastCost)) // if queued, or non-stablized least cost. 
+         {
+            lastCost = leastCost;
+            while (queuedCostResolutions.Count > 0)
+            {
+               queuedCostResolutions.Dequeue().ResolveCost();
+            }
+            CavernSpotDictionary.OrderByDescending(spot => spot.Value.ManhattanDistance).ToList().ForEach(spot => spot.Value.ResolveCost()); //possible queued resolutions
+            leastCost = CavernSpotDictionary[new PointDescription { X = 0, Y = 0 }].LowestCostFromHere;
+         }
+      }
+   }
+}
---
+++ AoC_2021/Day16_PacketDecoder.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2021
+{
+   class Day16_PacketDecoder : AoCodeModule
+   {
+      public Day16_PacketDecoder()
+      {
+
+         inputFileName = @"InputFiles\" + this.GetType().Name + ".txt";
+         GetInput();
+         OutputFileReset();
+      }
+      public override void DoProcess()
+      {
+         //** > Result for Day16_PacketDecoder part 1: Summation of version info is 873 (Process: 3.3481 ms)
+         //** > Result for Day16_PacketDecoder part 2: Answer after processing: 402817863665 (Process: 0.0006 ms)
+         ResetProcessTimer(true);
+         BITSTransmission transmission = new BITSTransmission(inputFile[0]);
+         transmission.Setup();
+         transmission.Decode();
+         AddResult("Summation of version info is " + transmission.versionSummation);
+         ResetProcessTimer(true);
+         AddResult("Answer after processing: " + transmission.AnswerValue);
+         ResetProcessTimer(true);
+
+      }
+   }
+   public class BITSTransmission
+   {
+      public enum operation { sum, product, min, max, literal, gt, lt, eq }
+      public string binaryRepresentation = "";
+      public string initialHex = "";
+      public int version;
+      public int typeId;
+      public int lengthTypeId;
+
+
+      public int recursionLevel = 0;
+      public bool displayOutput = false;
+      public long versionSummation = 0;
+      public long RegisterValue;
+      public long AnswerValue;
+      public operation currentOperation;
+      public bool firstSet;
+      public BITSTransmission(string hexInput)
+      {
+         initialHex = hexInput;
+         binaryRepresentation = "";
+         version=0;
+         typeId=0;
+         lengthTypeId=0;
+         recursionLevel = 0;
+         versionSummation = 0;
+         DebugInfo(initialHex);
+      }
+      public void Setup()
+      {
+         string hex, binaryVersion;
+         int dec;
+         for (int x = 0; x < initialHex.Length; x++)
+         {
+            hex = initialHex[x].ToString();
+            dec = Convert.ToInt32(hex, 16);
+            binaryVersion = Convert.ToString(dec, 2).PadLeft(4, '0');
+            binaryRepresentation += binaryVersion;
+         }
+         DebugInfo("Remaining: " + binaryRepresentation);
+         DebugInfo("Length is " + binaryRepresentation.Length + " which matches expected: " + ((initialHex.Length*4) == binaryRepresentation.Length).ToString());
+         SetVersionAndType();
+      }
+      public int SetVersionAndType()
+      {
+         string versionBits = GetBits(3);
+         string typeBits = GetBits(3);
+         if (versionBits != "")
+         {
+            version = Convert.ToInt32(versionBits, 2); 
+            versionSummation += version;
+         }
+         else
+         { version = 0; }
+         if (typeBits != "")
+         {
+            typeId = Convert.ToInt32(typeBits, 2);
+         }
+         else
+         { typeId = 0; }
+         DebugInfo("Version is " + version);
+         DebugInfo("Type is " + typeId);
+         return versionBits.Length + typeBits.Length;
+      }
+      public int Decode()
+      {
+         recursionLevel++;
+         DebugInfo("Entering Decode.");
+         int bitsProcessed = 0;
+         recursionLevel++;
+         switch ((operation)typeId)
+         {
+            case operation.literal:
+               DebugInfo("Type is Literal(type 4)");
+               string chunk;
+               string decoded = "";
+               bool endOfPacketMarked = false;
+               while (!endOfPacketMarked)
+               {
+                  chunk = GetBits(5);
+                  bitsProcessed += 5;
+                  endOfPacketMarked = chunk[0] == '0';
+                  decoded += chunk.Substring(1);
+               }
+               long literalvalue = Convert.ToInt64(decoded, 2);
+               AnswerValue = literalvalue;
+               
+               firstSet = false;
+               DebugInfo("Literal Decoded:" + literalvalue.ToString());
+               break;
+            default:
+               AnswerValue = 0;
+               currentOperation = (operation)typeId;
+               firstSet = true;
+               bitsProcessed += ProcessSubpackets();
+               RegisterValue = AnswerValue;
+               break;
+         }
+         recursionLevel--;
+         DebugInfo("Exiting Decode");
+         recursionLevel--;
+         return bitsProcessed;
+      }
+      public int ProcessSubpackets()
+      {
+         int bitsProcessed = 0;
+         int fieldLength = GetBits(1) == "0" ? 15 : 11; bitsProcessed += 1;
+         DebugInfo("Mode is " + (fieldLength == 15 ? "Total Number Of Bits in Sub-packet(not iteration of subpackets). Should read 15 bits." : "Total number of subpackets to process. Should read 11 bits."));
+         int operatorLength = Convert.ToInt32(GetBits(fieldLength), 2); bitsProcessed += fieldLength;
+         DebugInfo("Field Length is " + fieldLength.ToString());
+         DebugInfo((fieldLength == 15 ? "Total Number Of Bits in Sub-packet:" : "Total number of subpackets to process:") + operatorLength.ToString());
+         operation thisSubpacketOp = currentOperation;
+         long thisSubpacketAnswer = 0;
+         bool thisFirstSet = true;
+         if (fieldLength == 11)
+         {
+            DebugInfo("Iterating Subpackets.");
+            long thisRegister = 0;
+            for (int x = 0; x < operatorLength; x++)
+            {
+               int currentType = typeId;
+               int currentVersion = version;
+               bitsProcessed += SetVersionAndType();
+               bitsProcessed += Decode();
+               long SavedAnswer = AnswerValue;
+               DebugInfo("Answer From Subpacket: " + SavedAnswer);
+               DebugInfo("Current Operation is " + thisSubpacketOp);
+               switch (thisSubpacketOp)
+               {
+                  case operation.sum:
+                     thisSubpacketAnswer += SavedAnswer;
+                     break;
+                  case operation.product:
+                     thisSubpacketAnswer = (thisFirstSet ? 1 : thisSubpacketAnswer) * SavedAnswer;
+                     break;
+                  case operation.min:
+                     thisSubpacketAnswer = Math.Min((thisFirstSet ? long.MaxValue : thisSubpacketAnswer), SavedAnswer);
+                     break;
+                  case operation.max:
+                     thisSubpacketAnswer = Math.Max((thisFirstSet ? long.MinValue : thisSubpacketAnswer), SavedAnswer);
+                     break;
+                  case operation.literal:
+                     thisSubpacketAnswer = SavedAnswer;
+                     break;
+                  case operation.gt:
+                     if (thisFirstSet)
+                        thisRegister = SavedAnswer;
+                     else
+                        thisSubpacketAnswer = thisRegister > SavedAnswer ? 1 : 0;
+                     break;
+                  case operation.lt:
+                     if (thisFirstSet)
+                        thisRegister = SavedAnswer;
+                     else
+                        thisSubpacketAnswer = thisRegister < SavedAnswer ? 1 : 0;
+                     break;
+                  case operation.eq:
+                     if (thisFirstSet)
+                        thisRegister = SavedAnswer;
+                     else
+                        thisSubpacketAnswer = thisRegister == SavedAnswer ? 1 : 0;
+                     break;
+
+               }
+               thisFirstSet = false;
+               typeId = currentType;
+               version = currentVersion;
+            }
+
+         }
+         else
+         {
+            DebugInfo("Processing by bit count.");
+            int operationBitCount = 0;
+            int currentType = typeId;
+            int currentVersion = version;
+            int bitCounter;
+            long thisRegister = 0;
+            while (operationBitCount < operatorLength)
+            {
+               bitCounter = SetVersionAndType();
+               bitsProcessed += bitCounter;
+               operationBitCount += bitCounter;
+               
+               if (operationBitCount < operatorLength)
+               {
+                  bitCounter = Decode();
+                  long SavedAnswer = AnswerValue;
+                  
+                  DebugInfo("Answer From Subpacket: " + SavedAnswer);
+                  DebugInfo("Current Operation is " + thisSubpacketOp);
+                  switch (thisSubpacketOp)
+                  {
+                     case operation.sum:
+                        thisSubpacketAnswer += SavedAnswer;
+                        break;
+                     case operation.product:
+                        thisSubpacketAnswer = (thisFirstSet ? 1 : thisSubpacketAnswer) * SavedAnswer;
+                        break;
+                     case operation.min:
+                        thisSubpacketAnswer = Math.Min((thisFirstSet ? long.MaxValue : thisSubpacketAnswer), SavedAnswer);
+                        break;
+                     case operation.max:
+                        thisSubpacketAnswer = Math.Max((thisFirstSet ? long.MinValue : thisSubpacketAnswer), SavedAnswer);
+                        break;
+                     case operation.literal:
+                        thisSubpacketAnswer = SavedAnswer;
+                        break;
+                     case operation.gt:
+                        if (thisFirstSet)
+                           thisRegister = SavedAnswer;
+                        else
+                           thisSubpacketAnswer = thisRegister > SavedAnswer ? 1 : 0;
+                        break;
+                     case operation.lt:
+                        if (thisFirstSet)
+                           thisRegister = SavedAnswer;
+                        else
+                           thisSubpacketAnswer = thisRegister < SavedAnswer ? 1 : 0;
+                        break;
+                     case operation.eq:
+                        if (thisFirstSet)
+                           thisRegister = SavedAnswer;
+                        else
+                           thisSubpacketAnswer = thisRegister == SavedAnswer ? 1 : 0;
+                        break;
+
+                  }
+                  thisFirstSet = false;
+                  operationBitCount += bitCounter;
+                  bitsProcessed += bitCounter;
+               }
+
+            }
+            typeId = currentType;
+            version = currentVersion;
+         }
+         AnswerValue = thisSubpacketAnswer;
+         return bitsProcessed;
+      }
+      public string GetBits(int count)
+      {
+         if (binaryRepresentation.Length >= count)
+         {
+            string bits = binaryRepresentation.Substring(0, count);
+            binaryRepresentation = binaryRepresentation.Substring(count);
+            DebugInfo("Read " + count + " bits " + bits);
+            return bits;
+         }
+         else if (binaryRepresentation.Length > 0)
+         {
+            count = binaryRepresentation.Length;
+            return GetBits(count);
+         }
+         else
+            return "";
+      }
+      public void DebugInfo(string text)
+      {
+         if (displayOutput)
+         {
+            string tabs = new string('\t', recursionLevel>0?recursionLevel - 1:0);
+            Console.WriteLine(tabs + text);
+         }
+      }
+
+   }
+}
