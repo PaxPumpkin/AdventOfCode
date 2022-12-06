f7d87c4fc38cbe8503d523db628890727bfc9e70
Demetrio DuPlissey <dduplissey@genevasi.com>
2022-12-06 06:30:25 -0500

    Day 06 - trying some things seen on reddit, did some cleanup

--- AoC_2022/Day06_TuningTrouble.cs
+++ AoC_2022/Day06_TuningTrouble.cs
@@ -18,35 +18,38 @@ namespace AoC_2022
         {
             //** > Result for Day06_TuningTrouble part 1: Unique Signal Pattern Found at character 1235 (Process: 0.5717 ms)
             //** > Result for Day06_TuningTrouble part 2: Start of Message Marker Found at character 3051 (Process: 6.5706 ms)
-            string finalResult;
+            string finalResult, signal = inputFile[0];
+            int PACKET_MARKER = 4, MESSAGE_MARKER = 14; // unique char count to qualify as marker
             ResetProcessTimer(true);
-            finalResult = "Unique Signal Pattern Found at character " + ScanSignal(inputFile[0], 4);
-            AddResult(finalResult); 
-            ResetProcessTimer(true);
-            finalResult = "Start of Message Marker Found at character " + ScanSignal(inputFile[0], 14);
-            AddResult(finalResult); 
-            ResetProcessTimer(true);
+
+            finalResult = "Start of Packet Marker Found at character: " + ScanSignal(signal, PACKET_MARKER); 
+            // saw this style on reddit, but over multiple runs, it is consistently and notably slower.
+            //finalResult = signal.Select((x, i) => (signal.Skip(i).Take(PACKET_MARKER).Distinct().Count() == PACKET_MARKER, i + PACKET_MARKER)).First(x => x.Item1).Item2.ToString();
+            AddResult(finalResult); ResetProcessTimer(true);
+
+            finalResult = "Start of Message Marker Found at character: " + ScanSignal(signal, MESSAGE_MARKER); 
+            //finalResult = signal.Select((x, i) => (signal.Skip(i).Take(MESSAGE_MARKER).Distinct().Count() == MESSAGE_MARKER, i + MESSAGE_MARKER)).First(x => x.Item1).Item2.ToString();
+            AddResult(finalResult); ResetProcessTimer(true);
+
         }
-        public string ScanSignal(string stream, int headerMarkerSize)
+        public string ScanSignal(string stream, int markerLength)
         {
             Queue<char> signalStream = new Queue<char>();
-            int charCounter = 0;
+            int streamCounter = 0;
             string processedStreamLength = "Not Found";
             foreach (char c in stream)
             {
-                charCounter++;
+                streamCounter++;
                 signalStream.Enqueue(c);
-                if (signalStream.Count > headerMarkerSize)
+                if (signalStream.Count > markerLength) signalStream.Dequeue(); // keep window on stream processing to exact size of marker
+                //if (signalStream.Distinct().Count() == markerLength) // saw this on reddit, it also had odd standouts in taking longer to run than the group by (which I really don't get, at all, but it's true)
+                if (signalStream.GroupBy(ch => ch).Count() == markerLength) // group by gives number of unique characters in queue, if it matches our target length, we found it. 
                 {
-                    signalStream.Dequeue();
-                }
-                if (signalStream.GroupBy(ch => ch).Count() == headerMarkerSize)
-                {
-                    processedStreamLength = charCounter.ToString();
+                    processedStreamLength = streamCounter.ToString();
+                    signalStream.Clear();
                     break;
                 }
             }
-            signalStream.Clear();
             return processedStreamLength;
         }
     }
