10bc6d81c886d4b05947b03458c59342704fa849
Demetrio DuPlissey <dduplissey@genevasi.com>
2022-12-09 12:12:24 -0500

    Days 8 and 9

---
+++ AoC_2022/Day08_TreetopTreeHouse.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2022
+{
+    class Day08_TreetopTreeHouse : AoCodeModule
+    {
+        public Day08_TreetopTreeHouse()
+        {
+            inputFileName = @"InputFiles\" + this.GetType().Name + ".txt";
+            GetInput(); 
+            OutputFileReset();
+        }
+        public override void DoProcess()
+        {
+            //** > Result for Day08_TreetopTreeHouse part 1: Trees Visible: 1546 (Process: 17.3167 ms)
+            //** > Result for Day08_TreetopTreeHouse part 2: Max Scenic Score: 519064 (Process: 116.2012 ms)
+            /**
+             * TODO
+             * SERIOUSLY MESSY and INEFFICIENT
+             * REFACTOR THIS NONSENSE
+             */
+            string finalResult = "Not Set";
+            ResetProcessTimer(true);
+            List<TreeTopRow> trees = new List<TreeTopRow>();
+            foreach (string processingLine in inputFile)
+            {
+                if (!processingLine.Equals(string.Empty))
+                    trees.Add(new TreeTopRow(processingLine));
+            }
+            int visibleTrees = 0;
+            for (int rc =1; rc < trees.Count-1; rc++)
+            {
+                for(int colCounter = 1; colCounter< trees[rc].Row.Length-1; colCounter++)
+                {
+                    string row = trees[rc].Row;
+                    int thisTree = row[colCounter];
+                    visibleTrees += row.Take(colCounter).All(t => t < thisTree) 
+                        || row.Skip(colCounter+1).Take(row.Length - colCounter).All(t => t < thisTree) 
+                        || trees.Take(rc).All(t=> t.Row[colCounter]< thisTree )
+                        || trees.Skip(rc + 1).Take(trees.Count - rc).All(t => t.Row[colCounter] < thisTree)
+                        ? 1 : 0;
+                }
+            }
+            visibleTrees += (trees[0].Row.Length * 2) + (trees.Count * 2) - 4;
+            finalResult = "Trees Visible: " + visibleTrees.ToString();
+            AddResult(finalResult);
+            ResetProcessTimer(true);
+            int rowCounter = 0;
+            foreach(TreeTopRow row in trees)
+            {
+                foreach(TreeTopTree tree in row.trees)
+                {
+                    if (rowCounter == 0 || rowCounter == trees.Count - 1 || tree.Column == 0 || tree.Column == row.Row.Length - 1) { 
+                        
+                        tree.ScenicScore = 0;  continue; 
+                    }
+                    int score = 1;
+                    int treeCount = 0;
+                    string treeString = "";
+                    treeString = new string(row.Row.Take(tree.Column).Reverse().ToArray());// ToString();
+                    treeCount = treeString.IndexOf(treeString.FirstOrDefault(t => t >= tree.Height))+1;
+                    score *= treeCount == 0 ? treeString.Length : treeCount;
+                    treeCount = 0;
+                    treeString = new string(row.Row.Skip(tree.Column + 1).Select(c => c).ToArray());// ToString();
+                    treeCount = treeString.IndexOf(treeString.FirstOrDefault(t => t >= tree.Height)) + 1;
+                    score *= treeCount == 0 ? treeString.Length : treeCount;
+                    treeCount = 0;
+                    treeString = "";
+                    for(int c = 0; c<rowCounter; c++)
+                    {
+                        treeString += trees[c].Row[tree.Column];
+                    }
+                    treeString = new string(treeString.Reverse().ToArray());
+                    treeCount = treeString.IndexOf(treeString.FirstOrDefault(t => t >= tree.Height)) + 1;
+                    score *= treeCount == 0 ? treeString.Length : treeCount;
+                    treeCount = 0;
+                    treeString = "";
+                    for (int c = rowCounter+1; c < trees.Count; c++)
+                    {
+                        treeString += trees[c].Row[tree.Column];
+                    }
+                    treeCount = treeString.IndexOf(treeString.FirstOrDefault(t => t >= tree.Height)) + 1;
+                    score *= treeCount == 0 ? treeString.Length : treeCount;
+                    treeCount = 0;
+                    treeString = "";
+                    tree.ScenicScore = score;
+                }
+                rowCounter++;
+            }
+            finalResult = "Max Scenic Score: " + trees.Max(treeRow => treeRow.trees.Max(tree => tree.ScenicScore)).ToString();
+            AddResult(finalResult);
+            ResetProcessTimer(true);
+        }
+    }
+    public class TreeTopRow
+    {
+        public string Row;
+        public List<TreeTopTree> trees = new List<TreeTopTree>();
+        public TreeTopRow(string row)
+        {
+            Row = row;
+            int idx = 0;
+            foreach(char c in row)
+            {
+                trees.Add(new TreeTopTree() { Column = idx, Height = c });
+                idx++;
+            }
+        }
+    }
+    public class TreeTopTree
+    {
+        public int Column;
+        public char Height;
+        public long ScenicScore;
+    }
+}
---
+++ AoC_2022/Day09_RopeBridge.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2022
+{
+    class Day09_RopeBridge : AoCodeModule
+    {
+        public Day09_RopeBridge()
+        {
+            inputFileName = @"InputFiles\" + this.GetType().Name + ".txt";
+            GetInput();
+            OutputFileReset();
+        }
+        public override void DoProcess()
+        {
+            //** > Result for Day09_RopeBridge part 1: 2 Knots Number of Places Tail Visited Once: 6011 (Process: 1.9518 ms)
+            //** > Result for Day09_RopeBridge part 2: 10 Knots Number of Places Tail Visited Once: 2419 (Process: 4.4202 ms)
+            ResetProcessTimer(true);
+            foreach(int KnotCount in new int[]{ 2, 10 }) { AddResult(RunProblemForKnotCount(KnotCount, inputFile)); ResetProcessTimer(true); }
+        }
+        public string RunProblemForKnotCount(int numberOfKnots, List<string> inputFile)
+        {
+            List<(int X, int Y)> Knots = new List<(int X, int Y)>();
+            for (int x = 1; x <= numberOfKnots; x++) Knots.Add((0, 0));
+            HashSet<(int X, int Y)> TailVisits = new HashSet<(int X, int Y)>() { Knots.Last() };
+            foreach (string processingLine in inputFile) ProcessMovementCommand(processingLine, Knots, TailVisits);
+            return Knots.Count.ToString() + " Knots Number of Places Tail Visited Once: " + TailVisits.Count.ToString();
+        }
+        public void ProcessMovementCommand(string command, List<(int X, int Y)> knots, HashSet<(int X, int Y)> tailVisits)
+        {
+            string[] parsed = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string direction = parsed[0];
+            int numberOfSteps = int.Parse(parsed[1]);
+            int xMovement, yMovement;
+            (int X, int Y) head;
+            for (int step = 0; step < numberOfSteps; step++)
+            {
+                switch (direction)
+                {
+                    case "U":
+                        xMovement = 1; yMovement = 0;
+                        break;
+                    case "D":
+                        xMovement = -1; yMovement = 0;
+                        break;
+                    case "L":
+                        xMovement = 0; yMovement = -1;
+                        break;
+                    case "R":
+                        xMovement = 0; yMovement = 1;
+                        break;
+                    default:
+                        xMovement = 0; yMovement = 0;
+                        break;
+                }
+                head = knots.First();
+                head.X += xMovement; head.Y += yMovement;
+                knots[0] = head; // tuple is by value, not by ref, so gotta put it back to retain new values.
+                for (int x = 1; x < knots.Count; x++)
+                {
+                    (int X, int Y) testKnot = knots[x];
+                    int diffX = head.X - testKnot.X, diffY = head.Y - testKnot.Y;
+                    if (Math.Abs(diffX) > 1 || Math.Abs(diffY) > 1)
+                    {
+                        testKnot.X += diffX < 0 ? -1 : diffX > 0 ? 1 : 0;
+                        testKnot.Y += diffY < 0 ? -1 : diffY > 0 ? 1 : 0;
+                    }
+                    head = testKnot; // new "head" to compare against next knot.
+                    knots[x] = testKnot; 
+                }
+                tailVisits.Add(head); // after looping, head should be last knot - the tail. Add its current location to the list of visited spots. Hashset prevents duplication/overcounting inherently.
+            }
+        }
+    }
+}
